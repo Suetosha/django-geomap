af3ac3b23e5fb605fcb37e3f1fb20f4f9f344685
Suetosha <honey.pancake2017@yandex.ru>
2025-10-21 20:53:56 +0200

    Handle PlaceImage post-delete cleanup and update image upload path

--- places/models.py
+++ places/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 class Place(models.Model):
@@ -19,7 +21,7 @@ class Place(models.Model):
 
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, related_name="images", on_delete=models.CASCADE)
-    image = models.ImageField("Изображение", upload_to="media/")
+    image = models.ImageField("Изображение", upload_to="images/")
 
     def __str__(self):
         return f"Изображение для {self.place.title}"
@@ -27,3 +29,9 @@ class PlaceImage(models.Model):
     class Meta:
         verbose_name = "Изображение места"
         verbose_name_plural = "Изображения мест"
+
+
+@receiver(post_delete, sender=PlaceImage)
+def delete_image_file(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.delete(False)
